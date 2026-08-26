519523250628a1198e8dc7359b3694f470066299
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-08-26 14:17:32 +0300

    Add refresh token store to database context

--- STAJ/Data/AppDbContext.cs
+++ STAJ/Data/AppDbContext.cs
@@ -1,18 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using STAJ.Entities;
 
 namespace STAJ.Data
 {
     public class AppDbContext : DbContext
     {
-        
-            public AppDbContext(DbContextOptions<AppDbContext> options)
-                : base(options)
-            {
-            }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
 
-            public DbSet<Musteri> Musteriler { get; set; }
+        public DbSet<Musteri> Musteriler { get; set; }
         public DbSet<Kullanici> Kullanicilar { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<RefreshToken>()
+                .HasIndex(x => x.Token)
+                .IsUnique();
+
+            modelBuilder.Entity<RefreshToken>()
+                .HasOne(x => x.User)
+                .WithMany(x => x.RefreshTokens)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
