9df8ff9e85fce4c6f46ee133091353337eb6be5c
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-08-27 15:08:58 +0300

    Expose cursor pagination through customer service

--- STAJ/Services/MusteriService.cs
+++ STAJ/Services/MusteriService.cs
@@ -12,29 +12,20 @@ namespace STAJ.Services
             _repository = repository;
         }
 
-        public void Ekle(Musteri musteri)
-        {
-            _repository.Ekle(musteri);
-        }
+        public void Ekle(Musteri musteri) => _repository.Ekle(musteri);
 
         public List<Musteri> Getir(string? search = null, string? sort = null, int page = 1, int pageSize = 5)
         {
             return _repository.Getir(search, sort, page, pageSize);
         }
 
-        public Musteri? IdyeGoreGetir(int id)
+        public List<Musteri> CursorIleGetir(int? lastId = null, int pageSize = 5)
         {
-            return _repository.IdyeGoreGetir(id);
+            return _repository.CursorIleGetir(lastId, pageSize);
         }
 
-        public void Guncelle(Musteri musteri)
-        {
-            _repository.Guncelle(musteri);
-        }
-
-        public void Sil(int id)
-        {
-            _repository.Sil(id);
-        }
+        public Musteri? IdyeGoreGetir(int id) => _repository.IdyeGoreGetir(id);
+        public void Guncelle(Musteri musteri) => _repository.Guncelle(musteri);
+        public void Sil(int id) => _repository.Sil(id);
     }
 }
