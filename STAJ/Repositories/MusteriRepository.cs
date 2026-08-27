fd7b3f683769a351dea5f4e986f27876d031c568
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-08-27 14:18:34 +0300

    Fix customer update by modifying tracked entity

--- STAJ/Repositories/MusteriRepository.cs
+++ STAJ/Repositories/MusteriRepository.cs
@@ -5,23 +5,34 @@ namespace STAJ.Repositories
 {
     public class MusteriRepository
     {
-        
-            private readonly AppDbContext _context;
+        private readonly AppDbContext _context;
 
-            public MusteriRepository(AppDbContext context)
-            {
-                _context = context;
-            }
+        public MusteriRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public void Ekle(Musteri musteri)
         {
             _context.Musteriler.Add(musteri);
             _context.SaveChanges();
         }
+
         public void Guncelle(Musteri musteri)
         {
-            _context.Musteriler.Update(musteri);
+            var mevcutMusteri = _context.Musteriler.Find(musteri.Id);
+
+            if (mevcutMusteri == null)
+                return;
+
+            mevcutMusteri.Ad = musteri.Ad;
+            mevcutMusteri.Soyad = musteri.Soyad;
+            mevcutMusteri.Telefon = musteri.Telefon;
+            mevcutMusteri.Email = musteri.Email;
+
             _context.SaveChanges();
         }
+
         public void Sil(int id)
         {
             var musteri = _context.Musteriler.Find(id);
@@ -32,10 +43,12 @@ namespace STAJ.Repositories
                 _context.SaveChanges();
             }
         }
+
         public Musteri? IdyeGoreGetir(int id)
         {
             return _context.Musteriler.Find(id);
         }
+
         public List<Musteri> Getir()
         {
             return _context.Musteriler.ToList();
