b00d293349d3d2a00799215f41d91d2334d96c31
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-08-27 12:16:21 +0300

    Add JWT authorization support to Swagger

--- STAJ/Program.cs
+++ STAJ/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using STAJ.Data;
 using STAJ.Middleware;
 using STAJ.Repositories;
@@ -59,7 +60,33 @@ builder.Services.AddControllers()
     });
 
 builder.Services.AddScoped<MusteriRepository>();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Access Token'ı buraya yapıştırın."
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 builder.Services.AddScoped<MusteriService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
