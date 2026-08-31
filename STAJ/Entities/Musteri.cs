eecc0c448554dc7768a7e098f0d1155186ba3c17
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-08-31 14:44:42 +0300

    Add optional birth date to customer

--- STAJ/Entities/Musteri.cs
+++ STAJ/Entities/Musteri.cs
@@ -20,6 +20,8 @@ namespace STAJ.Entities
         [EmailAddress(ErrorMessage = "Geçerli bir email adresi giriniz.")]
         public string Email { get; set; } = string.Empty;
 
+        public DateTime? DogumTarihi { get; set; }
+
         public string? ProfilFotoUrl { get; set; }
     }
 }
