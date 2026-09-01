419c4c92db8acd3185a8efe0c55ced53e168d5dd
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-01 09:40:38 +0300

    Use dedicated TcKimlikNo validator in customer validation

--- STAJ/Validators/MusteriValidator.cs
+++ STAJ/Validators/MusteriValidator.cs
@@ -14,22 +14,7 @@ namespace STAJ.Validators
             RuleFor(x => x.DogumTarihi).Must(t => !t.HasValue || t.Value.Date.AddYears(18) <= DateTime.UtcNow.Date).WithMessage("Müşteri en az 18 yaşında olmalıdır.");
 
             RuleFor(x => x.TcKimlikNo)
-                .NotEmpty().WithMessage("T.C. Kimlik No alanı zorunludur.")
-                .Matches("^[0-9]{11}$").WithMessage("T.C. Kimlik No 11 haneli ve sadece rakamlardan oluşmalıdır.")
-                .Must(x => string.IsNullOrEmpty(x) || x[0] != '0').WithMessage("T.C. Kimlik No'nun ilk hanesi 0 olamaz.")
-                .Must(GecerliTcKimlikNo).WithMessage("Geçerli bir T.C. Kimlik No giriniz.");
-        }
-
-        private static bool GecerliTcKimlikNo(string? tc)
-        {
-            if (string.IsNullOrWhiteSpace(tc) || tc.Length != 11 || !tc.All(char.IsDigit) || tc[0] == '0') return false;
-            var rakamlar = tc.Select(c => c - '0').ToArray();
-            var tekler = rakamlar[0] + rakamlar[2] + rakamlar[4] + rakamlar[6] + rakamlar[8];
-            var ciftler = rakamlar[1] + rakamlar[3] + rakamlar[5] + rakamlar[7];
-            var onuncu = ((tekler * 7) - ciftler) % 10;
-            if (onuncu < 0) onuncu += 10;
-            var onBirinci = rakamlar.Take(10).Sum() % 10;
-            return rakamlar[9] == onuncu && rakamlar[10] == onBirinci;
+                .SetValidator(new TcKimlikNoValidator());
         }
     }
 }
