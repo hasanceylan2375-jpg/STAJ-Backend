dd42a85e9353805f0939af7c4ad498b7fa2f2f80
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-02 14:46:51 +0300

    Use Unit of Work in customer service

--- STAJ/Services/MusteriService.cs
+++ STAJ/Services/MusteriService.cs
@@ -5,30 +5,44 @@ namespace STAJ.Services
 {
     public class MusteriService
     {
-        private readonly IMusteriRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public MusteriService(IMusteriRepository repository)
+        public MusteriService(IUnitOfWork unitOfWork)
         {
-            _repository = repository;
+            _unitOfWork = unitOfWork;
         }
 
-        public void Ekle(Musteri musteri) => _repository.Ekle(musteri);
+        public void Ekle(Musteri musteri)
+        {
+            _unitOfWork.Musteriler.Ekle(musteri);
+            _unitOfWork.SaveChanges();
+        }
 
         public bool TcKimlikNoVarMi(string tcKimlikNo, int? haricId = null)
-            => _repository.TcKimlikNoVarMi(tcKimlikNo, haricId);
+            => _unitOfWork.Musteriler.TcKimlikNoVarMi(tcKimlikNo, haricId);
 
         public List<Musteri> Getir(string? search = null, string? sort = null, int page = 1, int pageSize = 5)
         {
-            return _repository.Getir(search, sort, page, pageSize);
+            return _unitOfWork.Musteriler.Getir(search, sort, page, pageSize);
         }
 
         public List<Musteri> CursorIleGetir(int? lastId = null, int pageSize = 5)
         {
-            return _repository.CursorIleGetir(lastId, pageSize);
+            return _unitOfWork.Musteriler.CursorIleGetir(lastId, pageSize);
         }
 
-        public Musteri? IdyeGoreGetir(int id) => _repository.IdyeGoreGetir(id);
-        public void Guncelle(Musteri musteri) => _repository.Guncelle(musteri);
-        public void Sil(int id) => _repository.Sil(id);
+        public Musteri? IdyeGoreGetir(int id) => _unitOfWork.Musteriler.IdyeGoreGetir(id);
+
+        public void Guncelle(Musteri musteri)
+        {
+            _unitOfWork.Musteriler.Guncelle(musteri);
+            _unitOfWork.SaveChanges();
+        }
+
+        public void Sil(int id)
+        {
+            _unitOfWork.Musteriler.Sil(id);
+            _unitOfWork.SaveChanges();
+        }
     }
 }
