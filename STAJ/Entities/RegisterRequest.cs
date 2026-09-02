eb5742c06c09fed1deb17e43b5a36f5e626b3323
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-02 17:12:08 +0300

    Add registration request model

---
+++ STAJ/Entities/RegisterRequest.cs
@@ -0,0 +1,8 @@
+namespace STAJ.Entities
+{
+    public class RegisterRequest
+    {
+        public string KullaniciAdi { get; set; } = string.Empty;
+        public string Sifre { get; set; } = string.Empty;
+    }
+}
