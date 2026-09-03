4be92a5ea59f35ce7f800481b46f68275ab435bf
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-03 14:37:53 +0300

    Add request hash to idempotency records

--- STAJ/Entities/IdempotencyRecord.cs
+++ STAJ/Entities/IdempotencyRecord.cs
@@ -4,6 +4,7 @@ namespace STAJ.Entities
     {
         public int Id { get; set; }
         public string Key { get; set; } = null!;
+        public string RequestHash { get; set; } = null!;
         public string? Response { get; set; }
         public int StatusCode { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
