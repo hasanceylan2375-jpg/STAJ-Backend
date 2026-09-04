cdd792cda887def93d8a3c3319bec3db62dbd9fe
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-04 09:50:30 +0300

    Cache customer queries in memory

--- STAJ/Services/MusteriService.cs
+++ STAJ/Services/MusteriService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Caching.Memory;
 using STAJ.Entities;
 using STAJ.Repositories;
 
@@ -6,15 +7,33 @@ namespace STAJ.Services
     public class MusteriService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMemoryCache _cache;
+        private const string CacheVersionKey = "MusteriCacheVersion";
 
-        public MusteriService(IUnitOfWork unitOfWork)
+        public MusteriService(IUnitOfWork unitOfWork, IMemoryCache cache)
         {
             _unitOfWork = unitOfWork;
+            _cache = cache;
+        }
+
+        private int CacheVersion => _cache.GetOrCreate(CacheVersionKey, entry =>
+        {
+            entry.Priority = CacheItemPriority.NeverRemove;
+            return 1;
+        });
+
+        private void CacheTemizle()
+        {
+            _cache.Set(CacheVersionKey, CacheVersion + 1, new MemoryCacheEntryOptions
+            {
+                Priority = CacheItemPriority.NeverRemove
+            });
         }
 
         public void Ekle(Musteri musteri)
         {
             _unitOfWork.Musteriler.Ekle(musteri);
+            CacheTemizle();
         }
 
         public bool TcKimlikNoVarMi(string tcKimlikNo, int? haricId = null)
@@ -22,15 +41,33 @@ namespace STAJ.Services
 
         public List<Musteri> Getir(string? search = null, string? sort = null, int page = 1, int pageSize = 5)
         {
-            return _unitOfWork.Musteriler.Getir(search, sort, page, pageSize);
+            var cacheKey = $"Musteriler_{CacheVersion}_{search}_{sort}_{page}_{pageSize}";
+            return _cache.GetOrCreate(cacheKey, entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
+                return _unitOfWork.Musteriler.Getir(search, sort, page, pageSize);
+            })!;
         }
 
         public List<Musteri> CursorIleGetir(int? lastId = null, int pageSize = 5)
         {
-            return _unitOfWork.Musteriler.CursorIleGetir(lastId, pageSize);
+            var cacheKey = $"MusteriCursor_{CacheVersion}_{lastId}_{pageSize}";
+            return _cache.GetOrCreate(cacheKey, entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
+                return _unitOfWork.Musteriler.CursorIleGetir(lastId, pageSize);
+            })!;
         }
 
-        public Musteri? IdyeGoreGetir(int id) => _unitOfWork.Musteriler.IdyeGoreGetir(id);
+        public Musteri? IdyeGoreGetir(int id)
+        {
+            var cacheKey = $"Musteri_{id}";
+            return _cache.GetOrCreate(cacheKey, entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
+                return _unitOfWork.Musteriler.IdyeGoreGetir(id);
+            });
+        }
 
         public void Guncelle(Musteri musteri)
         {
@@ -39,6 +76,8 @@ namespace STAJ.Services
                 _unitOfWork.BeginTransaction();
                 _unitOfWork.Musteriler.Guncelle(musteri);
                 _unitOfWork.Commit();
+                _cache.Remove($"Musteri_{musteri.Id}");
+                CacheTemizle();
             }
             catch
             {
@@ -54,6 +93,8 @@ namespace STAJ.Services
                 _unitOfWork.BeginTransaction();
                 _unitOfWork.Musteriler.Sil(id);
                 _unitOfWork.Commit();
+                _cache.Remove($"Musteri_{id}");
+                CacheTemizle();
             }
             catch
             {
