3d77b61caf1a8176907e6a169d023ad8272884c7
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-09 15:33:27 +0300

    security: validate registration input lengths

--- STAJ/Entities/RegisterRequest.cs
+++ STAJ/Entities/RegisterRequest.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace STAJ.Entities
 {
     public class RegisterRequest
     {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string KullaniciAdi { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(128, MinimumLength = 8)]
         public string Sifre { get; set; } = string.Empty;
     }
 }
