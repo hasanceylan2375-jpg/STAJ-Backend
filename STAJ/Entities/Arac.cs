90ae58ba57440dd7212c836ed1be019418ea9e45
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-09 15:34:14 +0300

    security: limit vehicle input field lengths

--- STAJ/Entities/Arac.cs
+++ STAJ/Entities/Arac.cs
@@ -7,13 +7,17 @@ namespace STAJ.Entities
         public int Id { get; set; }
 
         [Required]
+        [StringLength(100)]
         public string Marka { get; set; } = string.Empty;
 
         [Required]
+        [StringLength(100)]
         public string Model { get; set; } = string.Empty;
 
         public int Yil { get; set; }
         public decimal Fiyat { get; set; }
+
+        [StringLength(2048)]
         public string? GorselUrl { get; set; }
     }
 }
