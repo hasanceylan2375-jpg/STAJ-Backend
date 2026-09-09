85f8a2739630b7ae544b41fef993e2adc15d1dac
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-09 15:33:34 +0300

    security: validate refresh token input length

--- STAJ/Entities/RefreshTokenRequest.cs
+++ STAJ/Entities/RefreshTokenRequest.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace STAJ.Entities
 {
     public class RefreshTokenRequest
     {
+        [Required]
+        [StringLength(256, MinimumLength = 32)]
         public string RefreshToken { get; set; } = string.Empty;
     }
 }
