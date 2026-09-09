b6652a203de4a1626a5fb48d4264072eadcc568e
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-09 14:55:48 +0300

    security: restrict mail endpoint to admins

--- STAJ/Controllers/MailController.cs
+++ STAJ/Controllers/MailController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using STAJ.Entities;
 using STAJ.Services;
@@ -6,6 +7,7 @@ namespace STAJ.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize(Policy = "AdminOnly")]
     public class MailController : ControllerBase
     {
         private readonly MailService _mailService;
