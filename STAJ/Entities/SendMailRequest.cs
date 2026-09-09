c41d51630558c8a65497b7981769759b4a2ab852
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-09 15:33:39 +0300

    security: validate mail request input

--- STAJ/Entities/SendMailRequest.cs
+++ STAJ/Entities/SendMailRequest.cs
@@ -1,9 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace STAJ.Entities
 {
     public class SendMailRequest
     {
+        [Required]
+        [EmailAddress]
+        [StringLength(320)]
         public string To { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(200)]
+        [RegularExpression(@"^[^\r\n]*$", ErrorMessage = "Konu alanında satır sonu karakterleri kullanılamaz.")]
         public string Subject { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(10000)]
         public string Body { get; set; } = string.Empty;
     }
 }
