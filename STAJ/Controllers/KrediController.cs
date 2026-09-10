704957b36edec0ad83648738637e3e547adeab45
hasanceylan2375-jpg <hasanceylan2375@gmail.com>
2026-09-10 09:28:14 +0300

    Ticari kredi API endpointi eklendi

---
+++ STAJ/Controllers/KrediController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using STAJ.Models;
+using STAJ.Services;
+namespace STAJ.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class KrediController : ControllerBase
+{
+ private readonly KrediOdemePlanService _service;
+ public KrediController(KrediOdemePlanService service)=>_service=service;
+ [HttpPost("odeme-plani")]
+ public ActionResult<KrediOdemePlanResponse> OdemePlani([FromBody]KrediOdemePlanRequest request){try{return Ok(_service.Hesapla(request));}catch(ArgumentException ex){return BadRequest(new{mesaj=ex.Message});}}
+}
